51986c9bea335b91f299d5fbb53f37bae3451144
MentseurFares <mentseur.fares@hotmail.fr>
2024-01-16 11:26:55 +0100

    scrapper terminé sauf la gestion des 100 pages max

--- initialise_scrapper.py
+++ initialise_scrapper.py
@@ -11,12 +11,15 @@ collection_urls = db['urls']
 
 #lecture des paramêtres
 
-if len(sys.argv) < 2:
-    print("Vous devez fournir une URL à scrapper !")
+if len(sys.argv) < 3:
+    print("Vous devez fournir une URL à scrapper et le scope du scrapper !")
     exit(1)
 url_to_scrap = sys.argv[1]
+scope = sys.argv[2]
+
 print("Url à scrapper : ",url_to_scrap)
+print("Scope : ",scope)
 
 # Enregistrement de l'url dans la collection URLs
 
-collection_urls.insert_one({'url': url_to_scrap,'status': 'to_be_scrapped'})
+collection_urls.insert_one({'url': url_to_scrap,'status': 'to_be_scrapped','scope':scope})
